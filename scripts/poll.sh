e9ae9229e746786434fe2c9320b22c75dc307fcf
Frank Scholten <frank.scholten@container-solutions.com>
2019-02-20 16:47:10 +0100

    Updated nginx ingress config so backend metadata can be hit. Poll.sh now polls metadata endpoint.

--- scripts/poll.sh
+++ scripts/poll.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-  POLL=$(curl -s -k -L 35.189.94.221/frontend | grep frontend)
+  POLL=$(curl -s -k -L 35.189.94.221/backend/api/v1/metadata | jq -r .hostname)
   echo "$(date) $POLL"
   sleep 0.001
 done
